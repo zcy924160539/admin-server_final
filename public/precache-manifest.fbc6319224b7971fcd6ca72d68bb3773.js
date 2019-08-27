3b986aa69ce5c7b45fefeff2d38fbb636f3a37ce
Jack Chao <924160539@qq.com>
2019-08-27 18:02:29 +0800

    server

---
+++ public/precache-manifest.fbc6319224b7971fcd6ca72d68bb3773.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dff6f3343a580abe95893c92dfe28f0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26d7210ba80af527e094",
+    "url": "/static/css/2.ecaba1d8.chunk.css"
+  },
+  {
+    "revision": "9856dc237ab6eb7f638c",
+    "url": "/static/css/main.1b397112.chunk.css"
+  },
+  {
+    "revision": "26d7210ba80af527e094",
+    "url": "/static/js/2.0fdc50a7.chunk.js"
+  },
+  {
+    "revision": "9856dc237ab6eb7f638c",
+    "url": "/static/js/main.00231425.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9161e238b798c444b732dbbb7420a09e",
+    "url": "/static/media/404.9161e238.png"
+  },
+  {
+    "revision": "ab45504baf89be7c7792b4426c4a6594",
+    "url": "/static/media/bg.ab45504b.jpg"
+  },
+  {
+    "revision": "ba1f87ecbc18b2f0a8b6e29e8676ac82",
+    "url": "/static/media/logo.ba1f87ec.png"
+  }
+]);
